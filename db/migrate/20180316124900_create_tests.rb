0fd06ddef0f2819229213f43d7399454bbcd3a80
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-03-24 12:40:38 +0000

    Module 5, attempt 3

--- db/migrate/20180316124900_create_tests.rb
+++ db/migrate/20180316124900_create_tests.rb
@@ -4,7 +4,7 @@ class CreateTests < ActiveRecord::Migration[5.1]
       t.string :title
       t.integer :level
       t.references :category
-      t.references :author, foreign_key: true
+      t.references :author
 
       t.timestamps
     end
