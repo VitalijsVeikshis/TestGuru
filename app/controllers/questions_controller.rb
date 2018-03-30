3400456159dbd69b7b67cf502284e557153dc397
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-03-30 20:15:05 +0100

    Module 7, small fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :find_test
 
-  resque_from ActiveRecord::RecordNotFound,
+  rescue_from ActiveRecord::RecordNotFound,
               with: :rescue_with_question_not_found
 
   def index
