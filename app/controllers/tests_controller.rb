656d9718384b93269b43f650587cadddfe3f9654
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-04-02 21:25:52 +0100

    Module 8, corrections in tests_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -9,10 +9,11 @@ class TestsController < ApplicationController
 
   def new
     @test = Test.new
+    @author = nil
   end
 
   def create
-    @test = Test.new(test_params)
+    @test = Test.new(complete_test_params)
 
     if @test.save
       redirect_to @test
@@ -21,7 +22,9 @@ class TestsController < ApplicationController
     end
   end
 
-  def edit; end
+  def edit
+    @author = User.find(@test.author_id)
+  end
 
   def update
     if @test.update(test_params)
@@ -44,5 +47,10 @@ class TestsController < ApplicationController
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+
+  def complete_test_params
+    author_id = User.by_email(params[:test][:author]).first.id
+    test_params.merge(author_id: author_id)
   end
 end
