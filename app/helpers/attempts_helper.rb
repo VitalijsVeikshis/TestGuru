430caf013c117d1d1afc8854c50f8ec3bd3dd378
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-04-05 12:26:29 +0100

    Module 9, added checkboxes in _attempt

--- app/helpers/attempts_helper.rb
+++ app/helpers/attempts_helper.rb
@@ -1,13 +1,27 @@
 module AttemptsHelper
-  def attempt_color(answer, attempt)
-    pick_color(answer) if attempt.answer == answer
+  def color(answer, attempt)
+    choice_of_color(answer) if answer == attempt.answer
   end
 
-  def pick_color(answer)
+  def choice_of_color(answer)
     if answer.correct
       'attempt-success-color'
     else
       'attempt-unsuccess-color'
+    end
+  end
+
+  def comment(answer, attempt)
+    choice_of_comment(answer) if answer == attempt.answer
+  end
+
+  private
+
+  def choice_of_comment(answer)
+    if answer.correct
+      'Correct'
+    else
+      'Wrong'
     end
   end
 end
