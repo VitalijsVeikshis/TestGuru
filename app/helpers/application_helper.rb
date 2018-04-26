377f1d0ad604b814c83d1e6710e3dc48a328204a
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-04-26 21:41:02 +0300

    Module 13, attempt 1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,15 +9,24 @@ module ApplicationHelper
     }
   }.freeze
 
+  FLASH_STYLES = {
+    alert: 'alert alert-danger',
+    notice: 'alert alert-success'
+  }.freeze
+
   def current_year
     Date.current.year
   end
 
   def github_url(author, repo)
-    link_to 'TestGuru', "https://github.com/#{author}/#{repo}", target: '_blank'
+    link_to 'TestGuru', "https://github.com/#{author}/#{repo}", target: '_blank', class: 'text-white'
   end
 
   def flash_message(type, message)
     FLASH_MESSAGES[type][message]
+  end
+
+  def flash_style(type)
+    FLASH_STYLES[type]
   end
 end
