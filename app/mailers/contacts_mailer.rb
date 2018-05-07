234e2157246eb2ac17fdf7af6fe8a8a6a295b845
VitaliyVeykshis <vitalijs.veikshis@gmail.com>
2018-05-08 00:01:41 +0300

    Module 17, added from to mailer

--- app/mailers/contacts_mailer.rb
+++ app/mailers/contacts_mailer.rb
@@ -2,6 +2,8 @@ class ContactsMailer < ActionMailer::Base
   def new_contact(contact, email)
     @contact = contact
 
-    mail to: email, subject: "Message from #{@contact.name}"
+    mail from: @contact.email,
+         to: email,
+         subject: "Message from #{@contact.name}"
   end
 end
